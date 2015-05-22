f80688041a6f69ae48d434ae801664264066d319
Sam Stavinoha <smlstvnh@gmail.com>
2015-05-21 20:32:58 -0500

    just some little utils

---
+++ chef/utils.py
@@ -0,0 +1,28 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Misc. utility functions."""
+
+import os
+
+
+def normpath(path):
+    """Normalize a path.
+
+    Expands ~'s, resolves relative paths, normalizes and returns
+    an absolute path.
+    """
+    return os.path.abspath(os.path.normpath(os.path.expanduser(path)))
+
+
+def splitter(iterable, chunksize=60):
+    """Split an iterable that supports indexing into chunks of 'chunksize'."""
+    return (l[0+i:chunksize+i] for i in range(0, len(iterable), chunksize))
