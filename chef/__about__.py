58a1c0b419b3b3582a44e896c0d6d3bdd70c9e92
Sam Stavinoha <smlstvnh@gmail.com>
2015-05-21 20:31:42 -0500

    I'm a real package now

---
+++ chef/__about__.py
@@ -0,0 +1,29 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+__all__ = [
+    '__title__',
+    '__summary__',
+    '__url__',
+    '__version__',
+    '__author__',
+    '__email__',
+    '__license__',
+]
+
+__title__ = "okchef"
+__summary__ = "okchef is a chef api client library"
+__url__ = "https://github.com/samstav/okchef"
+__version__ = "0.0.1"
+__author__ = "Sam Stavinoha"
+__email__ = "samuel.stavinoha@rackspace.com"
+__license__ = "Apache License, Version 2.0"
---
+++ chef/__init__.py
@@ -0,0 +1,52 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
+
+from .client import ChefClient
+from .__about__ import *
+
+__all__ = ['HOSTED_CHEF_API', 'ChefClient']
+
+HOSTED_CHEF_API = 'https://api.chef.io'
+_HERE = os.path.dirname(os.path.realpath(__file__))
+_HEAD_FILE = os.path.abspath(os.path.join(_HERE, os.pardir, '.git', 'HEAD'))
+
+
+def _read_commit_dot_txt():
+    commit_dot_text = os.path.join(_HERE, 'commit.txt')
+    if os.path.isfile(commit_dot_txt):
+        with open(commit_dot_txt) as sha:
+            return sha.read().strip()
+
+
+def _read_commit(path):
+    """Lookup HEAD (current checkout) commit hash.
+
+    Falls back to reading commit.txt in this dir.
+    """
+    if not os.path.isfile(path):
+        return _read_version_dot_txt()
+    with open(path) as head:
+        headref = head.read().strip()
+    if 'ref:' in headref:
+        link = headref.partition('ref:')[-1].strip()
+        follow = os.path.join(os.path.dirname(path), link)
+        return _read_commit(follow)
+    elif len(headref) == 40:
+        return headref
+    else:
+        raise StandardError("Cannot read %s for HEAD sha-1." % path)
+
+
+__commit__ = _read_commit(_HEAD_FILE)
+
